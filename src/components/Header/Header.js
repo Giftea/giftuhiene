972af4a6e215b1e543b8894c29009394588213b5
giftea <eleojogiftuhiene@gmail.com>
2021-10-29 12:31:21 +0100

    test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,7 +22,7 @@ const Header = () =>  (
       </li>
       <li>
         <Link href="#tech">
-          <NavLink>Technologies</NavLink>
+          <NavLink>Technologiez</NavLink>
         </Link>
       </li>        
       <li>
